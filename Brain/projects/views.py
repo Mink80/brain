53cec5e86b394705caa69aea719ba0e96ad56e99
Kai Sassmannshausen <kai@sassie.de>
2019-10-24 23:07:04 +0200

    added first projects functionality

--- Brain/projects/views.py
+++ Brain/projects/views.py
@@ -9,5 +9,18 @@ projects_blueprint = Blueprint('projects', __name__,
 
 @projects_blueprint.route('/')
 def index():
-    projects = Project.query.all()
+    all_customers = Customer.query.all()
+
+    projects = {}
+    for c in all_customers:
+        projects[c] = Project.query.filter_by(customer_id=c.id).all()
+
     return render_template('/projects/list.html', projects=projects)
+
+
+@projects_blueprint.route('/<project_id>')
+def project(project_id):
+
+    tasks = Task.query.filter_by(id=project_id)
+
+    return render_template('/projects/project.html', tasks=tasks)
