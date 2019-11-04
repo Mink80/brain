956aae5eeb801bed5e76ad9e5f6899ea9be492fd
Kai Sassmannshausen <kai@sassie.de>
2019-11-04 14:52:48 +0100

    added add partner functionality

--- Brain/partners/views.py
+++ Brain/partners/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for
 from Brain import db
 from Brain.models import Partner
+from Brain.partners.forms import PartnerForm
 
 partners_blueprint = Blueprint('partners', __name__,
                             template_folder='templates')
@@ -8,5 +9,15 @@ partners_blueprint = Blueprint('partners', __name__,
 
 @partners_blueprint.route('/', methods=['GET','POST'])
 def index():
+    form = PartnerForm()
     all_partners = Partner.query.all()
-    return render_template("/partners/list.html", partners=all_partners)
+    
+    if form.validate_on_submit():
+        partner = Partner(name=form.name.data,
+                          comment=form.comment.data)
+        db.session.add(partner)
+        db.session.commit()
+        return redirect(url_for('partners.index'))
+    
+    return render_template("/partners/list.html", partners=all_partners,
+                                                   form=form)
