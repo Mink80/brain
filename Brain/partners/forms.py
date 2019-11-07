7da30fa601df3b4532c613b5e1332ad5e3fc6b8a
Kai Sassmannshausen <kai@sassie.de>
2019-11-07 10:00:17 +0100

    added edit partner functionality

--- Brain/partners/forms.py
+++ Brain/partners/forms.py
@@ -1,13 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import *
 from wtforms.validators import DataRequired
+from Brain.models import Partner
 
 class PartnerForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()],
                                            render_kw={"placeholder": "Partner Name"})
-    
+
+    def validate_name(form, field):
+        p = Partner.query.filter_by(name=field.data).first()
+        if p and (not int(p.id) == int(form.edit_id.data)):
+            raise ValidationError('A Partner with that name already exists')
+
     comment = StringField("Comment", render_kw={"placeholder": "Comment (optional)"})
     edit_id = HiddenField()
-    submit = SubmitField("Add")
-
- 
+    submit = SubmitField("Save")
--- Brain/partners/views.py
+++ Brain/partners/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from Brain import db
 from Brain.models import Partner
 from Brain.partners.forms import PartnerForm
@@ -11,13 +11,41 @@ partners_blueprint = Blueprint('partners', __name__,
 def index():
     form = PartnerForm()
     all_partners = Partner.query.all()
-    
+
     if form.validate_on_submit():
         partner = Partner(name=form.name.data,
                           comment=form.comment.data)
         db.session.add(partner)
         db.session.commit()
         return redirect(url_for('partners.index'))
-    
+
     return render_template("/partners/list.html", partners=all_partners,
                                                    form=form)
+
+
+@partners_blueprint.route('/edit/<partner_id>', methods=['POST', 'GET'])
+def edit(partner_id):
+    to_edit = Partner.query.get(partner_id)
+    if not to_edit:
+        return render_template('400.html'), 400
+
+    form = PartnerForm(name=to_edit.name, comment=to_edit.comment,
+                        edit_id=to_edit.id)
+
+    if form.validate_on_submit():
+        to_edit.name = form.name.data
+        to_edit.comment = form.comment.data
+        db.session.add(to_edit)
+        db.session.commit()
+        flash('Partner edited', 'alert alert-success alert-dismissible fade show')
+        return redirect(url_for('partners.index'))
+
+    partners = Partner.query.all()
+    return render_template("/partners/list.html", edit_id=to_edit.id,
+                                                    form=form,
+                                                    partners=partners)
+
+
+@partners_blueprint.route('/delete/<partner_id>', methods=['POST', 'GET'])
+def delete(partner_id):
+    pass
