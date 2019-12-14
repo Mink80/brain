f7afb9a44b3dd31daed5291285d0617b796d0317
Kai Sassmannshausen <kai@sassie.de>
2019-12-14 02:34:55 +0100

    added first version of dashboard

--- brain.py
+++ brain.py
@@ -2,6 +2,7 @@
 #    A lightweight web-application for managing tasks in projects
 #
 #    Copyright 2019, Kai Sassmannshausen
+#    Source code repository: https://github.com/Mink80/brain
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -18,11 +19,53 @@
 
 from Brain import app
 from flask import render_template
-from Brain.models import Task
+
+from Brain.models import Task, HistoryItem, Project, Customer, Partner
+from sqlalchemy import or_
+from Brain.types import Type
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    tasks_urgent = Task.query.filter( or_(Task.type==Type.Task, Task.type==Type.Request)). \
+                        filter_by(deleted=False). \
+                        filter(Task.duedate!=None). \
+                        order_by(Task.duedate). \
+                        limit(5). \
+                        all()
+
+    tasks_oldest = Task.query.filter( or_(Task.type==Type.Task, Task.type==Type.Request)). \
+                            filter_by(deleted=False). \
+                            filter(Task.duedate==None). \
+                            order_by(Task.cdate). \
+                            limit(5). \
+                            all()
+
+    history_items = HistoryItem.query.order_by(HistoryItem.id.desc()).limit(5).all()
+
+    tasks_count = Task.query.filter_by(deleted=False).count()
+    # should "misc" projects be counted in? i let it in for now...
+    projects_count = Project.query.count()
+    customers_count = Customer.query.count()
+    partners_count = Partner.query.count()
+
+    open_tasks_count = Task.query.filter_by(deleted=False). \
+                                    filter_by(type=Type.Task). \
+                                    count()
+
+    open_requests_count = Task.query.filter_by(deleted=False). \
+                                    filter_by(type=Type.Request). \
+                                    count()
+
+    return render_template('home.html', tasks_urgent=tasks_urgent,
+                                        tasks_oldest=tasks_oldest,
+                                        history_items=history_items,
+                                        short_history_table=True,
+                                        tasks_count=tasks_count,
+                                        projects_count=projects_count,
+                                        customers_count=customers_count,
+                                        partners_count=partners_count,
+                                        open_tasks_count=open_tasks_count,
+                                        open_requests_count=open_requests_count)
 
 @app.errorhandler(404)
 def page_not_found(e):
