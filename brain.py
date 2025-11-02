600721f0cf1ecc4cc29ea01caa26b86ebf9f0d8b
Kai Sassmannshausen <ronin@uosagas.com>
2025-11-02 14:20:38 +0100

    set port to 8080

--- brain.py
+++ brain.py
@@ -31,4 +31,4 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-        app.run(debug=True, host='0.0.0.0', port=80)
+        app.run(debug=True, host='0.0.0.0', port=8080)
